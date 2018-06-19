c61ca241698c2a6fe095af98c0b184aeb19fb699
lisaco-ono <lisaco.ono.ii@gmail.com>
2018-06-19 18:56:15 +0900

    10秒がんばって5秒休むループ

--- client/main.js
+++ client/main.js
@@ -1,29 +1,32 @@
 import { Meteor } from 'meteor/meteor';
 import { Promise } from 'meteor/promise';
 
-new Promise(function (resolve) {
-    console.time("1");
-    Meteor.setTimeout(function () {
-        console.log("step1");
-        step1ok = 1;
-        resolve(step1ok);
-        console.timeEnd("1");
-    },5000);
-}).then(function (step1ok) {
-    console.time("2");
-    return new Promise(function (resolve) {
-        Meteor.setTimeout(function () {
-            console.log("step2");
-            step2ok = 1;
-            resolve(step2ok);
-            console.timeEnd("2");
-        },1000);
-    })
-}).then(function (step2ok) {
-    console.time("3");
-    Meteor.setTimeout(function () {
-        console.log("step3");
-        step3ok = "ok";
-        console.timeEnd("3");
-    },2000);
+new Promise(function (reso) {
+
+    function study_time(i) {
+        return new Promise(function (resolve, reject) {
+            Meteor.setTimeout(function () {
+                console.log("10秒経ったよ！");
+                resolve(i);
+            },10000);
+        }).then(function (count) {
+            return new Promise(function (resolve, reject) {
+                Meteor.setTimeout(function () {
+                    console.log("5秒経ったよ！");
+                    resolve(count + 1);
+                }, 5000);
+            })
+        }).then(function (count) {
+            if (count>10){
+                reso();
+            } else {
+                study_time(count);
+            }
+        });
+    }
+    study_time(0);
+
+}).then(function () {
+    // console.time("2");
+    console.log("おわり！");
 });
